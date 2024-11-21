2d1cc7425e2973e7b9b87480aaabe96cc3df9095
Matheus Ferreira <matheusfgsantos31@gmail.com>
2024-11-21 20:00:50 -0300

    criação de entidades

---
+++ src/main/java/domain/adress/Address.java
@@ -0,0 +1,30 @@
+package domain.adress;
+
+import domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "event_address")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/domain/cupon/Cupon.java
@@ -0,0 +1,32 @@
+package domain.cupon;
+
+import domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "event-cupon")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
